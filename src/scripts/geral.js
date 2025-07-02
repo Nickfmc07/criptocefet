159efd227c6fb15da1964cce8036f2b18790bd13
Nícolas Francisco Martins Chagas <125323611+Nickfmc07@users.noreply.github.com>
2025-07-01 22:12:25 -0300

    Dropdown deslisante e classes css auxiliares
    O tema do site agora é setado como claro por padrão;
    
    Conteiners dropdown deslisam ao aparecer/esconder;
    
    Dropdown agora usam classe auxiliar show para aparecer;
    
    Cridas classes auxiliares para: linhas laterais de escopo, cores, alinhamento de texto e próprio (justify-self);;
    
    criada estilização para checkboxes slide;
    
    agora o título principal do main tem um ID próprio para possibilitar descrições dropdown.

--- src/scripts/geral.js
+++ src/scripts/geral.js
@@ -1,9 +1,10 @@
 const containers = document.querySelectorAll(".container");
-
+/*
 let tema = window.matchMedia('(prefers-color-scheme: dark)').matches
   ? "tema-escuro"
   : "tema-claro";
-
+*/
+let tema = 'tema-claro'
 containers.forEach(container => {
   container.classList.add(tema);
 });
@@ -13,11 +14,8 @@ const dropdown = document.querySelectorAll(".dropdown-btn");
 dropdown.forEach((btn) => {
   btn.addEventListener("click", () => {
     const content = btn.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-    btn.children[0].classList.toggle('flipped');
+    content.classList.toggle("show");
+
+    btn.lastElementChild.classList.toggle("flipped");
   });
 });
